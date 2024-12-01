e820131ce667dd459615f34dedb8c322e7610c40
ADLaraF10 <andres.lara.ttc.038@unilibre.edu.co>
2024-11-30 23:08:09 -0500

    Calculadora Funcionl js y html

--- JS/Calculadora.js
+++ JS/Calculadora.js
@@ -1,52 +1,72 @@
 /*-----------------CALCULADORA-----------------------*/
 
-//VARIABLES
-let numero1 = "";
-let numero2 = "";
-let operador = "";
-let numero1completo = false;
-
-let expresion = prompt('escriba su expresion simple')
-
-document.write(hacerOperacion(expresion))
+/* VARIABLES */
 
 
+
+//variables para las operaciones
+
+let borrar = false
 
 
 //FUNCIONES
-function hacerOperacion(expresion){
-    datos = getDatos(expresion);
-    //console.log(datos.n1 + datos.op + datos.n2 )
+
+function hacerOperacion(){
+    var expresion = document.getElementById('expresion')
+    datos = getDatos()
+    console.log(datos.n1 + datos.op + datos.n2 )
     if(datos.op === "+"){
-        return datos.n1 + datos.n2; 
+        expresion.value = datos.n1 + datos.n2; 
     }else if(datos.op === "-"){
-        return datos.n1 - datos.n2;
+        expresion.value = datos.n1 - datos.n2;
     }else if(datos.op === "*"){
-        return datos.n1 * datos.n2;
+        expresion.value = datos.n1 * datos.n2;
     }else if(datos.op === "/"){
-        return datos.n1 / datos.n2;
+        expresion.value = datos.n1 / datos.n2;
+    }else{
+        expresion.value = "ilegalArgumrnt"
     }
     
+    if(!borrar){
+        borrar = true
+    }
 }
 
-function getDatos(expresion){
+function getDatos(){
+    var input = document.getElementById('expresion')
+    let expresion = input.value
+    let numero1 = "";
+    let numero2 = "";
+    let operador = "";
+    let numero1completo = false;
     for(i = 0; i < expresion.length ; i++){
         if(expresion[i] != "+" && expresion[i] != "-" && expresion[i] != "*" && expresion[i] != "/"){
-            //console.log(expresion[i])
+            console.log(expresion[i])
             if(!numero1completo){
                 numero1 += expresion[i];
             }else{
                 numero2 += expresion[i];
             }
         }else{
-            //console.log(expresion[i])
+            console.log(expresion[i])
             operador = expresion[i];
+            if(numero1completo === true){
+                return { n1: 0, n2: 0, op: "@"}
+            }
             numero1completo = true;
         }
     }
-    return dato = { n1: parseInt(numero1), n2:parseInt(numero2), op: operador}
+    return { n1: parseInt(numero1), n2:parseInt(numero2), op: operador}
 }
 
+function agregarCaracter(caracter){
+    var expresion = document.getElementById('expresion')
+    if(borrar){
+        expresion.value = ""
+        borrar = false
+    }
+    expresion.value += caracter;
+}
 
 
 
