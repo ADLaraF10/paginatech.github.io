1bb4b03700c83de448d534bdb0678d944c97c2fc
ADLaraF10 <andres.lara.ttc.038@unilibre.edu.co>
2024-12-01 17:13:01 -0500

    validacion de formulario, detalles y correcion de errores

--- JS/index.js
+++ JS/index.js
@@ -19,3 +19,24 @@ enlaces.forEach(enlace => {
     });
 });
 
+/* VALIDAR FORMULARIO */
+
+function validarFormularioMensaje(event) {
+    event.preventDefault()
+    const campos = document.querySelectorAll('#seccion_form input[type="text"],textArea'); 
+    console.log(campos);
+    let camposVacios = false;
+    campos.forEach(campo => {
+      console.log("Revisando campo...");
+      const span = document.getElementById('alerta');
+      if (campo.value.trim() === "") {
+        span.style.display = 'inline'
+        camposVacios = true;
+      }else{
+        span.style.display = 'none'
+      }
+
+      return camposVacios;
+    });
+}
+
